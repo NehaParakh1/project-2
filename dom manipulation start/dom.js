0aba56ec5601a87c59d2e22768b98e25ee3a0a82
NehaParakh1 <nehadugar3@gmail.com>
2023-01-19 22:03:06 +0530

    taskno5

--- dom manipulation start/dom.js
+++ dom manipulation start/dom.js
@@ -8,6 +8,7 @@
 //console.log(document.all[10]);
 
 //GETELEMENTBYID
+
 //console.log(document.getElementById("header-title"));
 var headerTitle=document.getElementById("header-title");
 //console.log(headerTitle);
@@ -16,16 +17,31 @@ var headerTitle=document.getElementById("header-title");
 //headerTitle.innerHTML="<h3>Hii</h3>"
 //var header=document.getElementById("main-header")
 //header.style.border="solid 3px #000";
+
+//GETELMENTSBYCLASSNAME
+
 //var title1=document.getElementsByClassName("title");
 //console.log(title1);
 //title1[0].style.fontWeight="bold";
 //title1[0].style.color="green";
 
-var list=document.getElementsByClassName("list-group-item")
-console.log(list);
+//var list=document.getElementsByClassName("list-group-item")
+//console.log(list);
 //list[2].style.backgroundColor="green";
-for(var i=0;i<list.length;i++){
-    list[i].style.fontWeight="bold";
-    list[i].style.color="green";
+//for(var i=0;i<list.length;i++){
+   // list[i].style.fontWeight="bold";
+    //list[i].style.color="green";
+//}
+//task no 5: (li item 5 was added for this task in index.html)
+//var newli=document.getElementsByClassName("list-group-newitem")
+//newli[0].style.color="blue"
+
+//GETELEMENTSBYTAGNAME
+var liitems=document.getElementsByTagName("li")
+console.log(liitems);
+liitems[4].style.backgroundColor="green";
+for(var i=0;i<liitems.length;i++){
+    liitems[i].style.fontWeight="bold";
+    liitems[4].style.color="yellow";
 }
 
